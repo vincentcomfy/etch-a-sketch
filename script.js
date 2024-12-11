3e062d5f65786cec95508c631c767c7ba5e876df
vincent <85801240+vincentcomfy@users.noreply.github.com>
2024-12-12 02:15:02 +0800

    added initial attempt to pull data from grid size slider and pass to grid generation function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+let slider = document.getElementById("gridSizeSlider");
+let sliderOutput = document.getElementById("gridSizeOutput");
+sliderOutput.innerHTML = slider.value; // Initial display
+
+slider.oninput = function() {
+  sliderOutput.innerHTML = this.value;
+  drawSketchpad(this.value);
+}
+
 function drawSketchpad(gridSize) {
-    const gridContainer = document.getElementById('grid');
-  
-    for (let row = 0; row < gridSize; row++) {
-      for (let col = 0; col < gridSize; col++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        gridContainer.appendChild(cell);
-      }
+  const gridContainer = document.getElementById('grid');
+  gridContainer.innerHTML = ""; // Clear previous grid (optional)
+
+  for (let row = 0; row < gridSize; row++) {
+    for (let col = 0; col < gridSize; col++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      gridContainer.appendChild(cell);
     }
   }
+}
+
+// testing
+drawSketchpad(4);
